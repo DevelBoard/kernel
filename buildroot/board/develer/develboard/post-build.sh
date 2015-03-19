41a15470e9fdc081c3ecbd0368e35a140ef4a5c7
Mirko Damiani <mirko@develer.com>
2015-03-19 15:04:39 +0100

    Add test for sdcard

--- buildroot/board/develer/develboard/post-build.sh
+++ buildroot/board/develer/develboard/post-build.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-ROOTFSDIR=$1
+ROOTFSDIR=output/target
+OUTPUTDIR=output/images
+BOARDDIR=board/develer/develboard
 
 echo "Prepend AT91 nand flash header to bootstrap"
-board/develer/develboard/at91_nand_header.sh \
-	output/images/sama5d4_xplained-nandflashboot-uboot-3.7.1.bin \
-	output/images/at91bootstrap.bin
+"$BOARDDIR"/at91_nand_header.sh \
+	"$OUTPUTDIR"/sama5d4_xplained-nandflashboot-uboot-3.7.1.bin \
+	"$OUTPUTDIR"/at91bootstrap.bin
+
+echo "Add rootfs additions"
+rsync -rlpDSv "$BOARDDIR"/rootfs-additions/ "$ROOTFSDIR"/
 
 echo "Done"
