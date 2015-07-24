c5ab16effd5f519bee74dbe90269bcba6940428b
Lorenzo Villani <lorenzo@villani.me>
2015-07-24 18:44:44 +0200

    Let buildroot manage the rootfs overlay

--- buildroot/board/develer/develboard/post-build.sh
+++ buildroot/board/develer/develboard/post-build.sh
@@ -12,7 +12,4 @@ echo "Prepend AT91 nand flash header to bootstrap"
 	"$OUTPUTDIR"/sama5d4_xplained-nandflashboot-uboot-3.7.1.bin \
 	"$OUTPUTDIR"/at91bootstrap.bin
 
-echo "Add rootfs additions"
-rsync -rlpDSv "$BOARDDIR"/rootfs-additions/ "$ROOTFSDIR"/
-
 echo "Done"
