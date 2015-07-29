b0df795d1c101c9270377d2cb9c1a300ba316fe4
Lorenzo Villani <lorenzo@villani.me>
2015-07-29 14:02:35 +0200

    Move genhostname to a package

---
+++ buildroot/package/genhostname/genhostname.mk
@@ -0,0 +1,21 @@
+################################################################################
+#
+# genhostname
+#
+################################################################################
+
+GENHOSTNAME_VERSION = 1.0.0
+GENHOSTNAME_LICENSE = MIT
+GENHOSTNAME_SITE = package/genhostname
+GENHOSTNAME_SITE_METHOD = local
+
+define GENHOSTNAME_BUILD_CMDS
+    $(MAKE) CC="$(TARGET_CC)" LD="$(TARGET_LD)" -C $(@D) genhostname
+endef
+
+define GENHOSTNAME_INSTALL_TARGET_CMDS
+    $(INSTALL) -D -m 0755 $(@D)/genhostname $(TARGET_DIR)/usr/sbin/genhostname
+    $(INSTALL) -D -m 0644 package/genhostname/genhostname.service $(TARGET_DIR)/etc/systemd/system/genhostname.service
+endef
+
+$(eval $(generic-package))
