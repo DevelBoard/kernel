6ce0fc47046f52737cf1eecb02780266ef9f6ad5
Lorenzo Villani <lorenzo@villani.me>
2015-08-15 15:47:47 +0200

    Add and enable Connman, disable systemd-networkd

--- buildroot/board/develer/develboard/post-build.sh
+++ buildroot/board/develer/develboard/post-build.sh
@@ -40,9 +40,11 @@ if grep -q 'BR2_INIT_SYSTEMD=y' "${BR2_CONFIG}"; then
     ln -sf "$ROOTFSDIR/etc/systemd/system/genhostname.service" "$ROOTFSDIR/etc/systemd/system/multi-user.target.wants/genhostname.service"
 
     echo "Disable unnecessary services"
+    find "$ROOTFSDIR" -type f -iname 'network.service' -delete
     find "$ROOTFSDIR" -type f -iname 'systemd-hwdb-update.service' -delete
     find "$ROOTFSDIR" -type f -iname 'systemd-logind.service' -delete
     find "$ROOTFSDIR" -type f -iname 'systemd-remount-fs.service' -delete
+    find "$ROOTFSDIR" -type f -iname 'systemd-resolved.service' -delete
     find "$ROOTFSDIR" -type f -iname 'systemd-udev-hwdb-update.service' -delete
     find "$ROOTFSDIR" -type f -iname 'systemd-user-sessions.service' -delete
 fi
