10e670c13e07cc3922cc1378d5a7327247e93a16
Giovanni Bajo <rasky@develer.com>
2015-09-13 22:47:54 +0200

    Make rootfs tarball more lenient to binary diffs.
    This is also useful when committing the files into git.

--- buildroot/fs/common.mk
+++ buildroot/fs/common.mk
@@ -45,7 +45,7 @@ ROOTFS_$(2)_DEPENDENCIES += host-fakeroot host-makedevs \
 
 ifeq ($$(BR2_TARGET_ROOTFS_$(2)_GZIP),y)
 ROOTFS_$(2)_COMPRESS_EXT = .gz
-ROOTFS_$(2)_COMPRESS_CMD = gzip -9 -c
+ROOTFS_$(2)_COMPRESS_CMD = gzip --rsyncable -9 -c
 endif
 ifeq ($$(BR2_TARGET_ROOTFS_$(2)_BZIP2),y)
 ROOTFS_$(2)_COMPRESS_EXT = .bz2
