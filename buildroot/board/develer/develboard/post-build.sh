6fbff82c55db0af9f50db2002542e224d9cb8489
Giovanni Bajo <rasky@develer.com>
2015-09-17 18:37:40 +0200

    Cleanup and rename image files in output directory

--- buildroot/board/develer/develboard/post-build.sh
+++ buildroot/board/develer/develboard/post-build.sh
@@ -9,8 +9,19 @@ BOARDDIR="$PWD/board/develer/develboard"
 
 echo "Prepend AT91 nand flash header to bootstrap"
 "$BOARDDIR"/at91_nand_header.sh \
-	"$OUTPUTDIR"/sama5d4_xplained-nandflashboot-uboot-3.7.1.bin \
-	"$OUTPUTDIR"/at91bootstrap.bin
+	"$OUTPUTDIR"/sama5d4_xplained-nandflashboot-barebox-*.bin \
+	"$OUTPUTDIR"/.prefix-bootloader.bin
+
+mv "$OUTPUTDIR"/.prefix-bootloader.bin \
+    "$OUTPUTDIR"/develboard-nandflashboot-barebox.bin
+
+mv "$OUTPUTDIR"/sama5d4_xplained-sdcardboot-barebox-*.bin \
+    "$OUTPUTDIR"/develboard-sdcardboot-barebox.bin
+
+rm "$OUTPUTDIR"/sama5d4_xplained-*.bin
+rm "$OUTPUTDIR"/at91bootstrap.bin
+
+mv "$OUTPUTDIR"/barebox-*-env "$OUTPUTDIR"/barebox.env || true
 
 #
 # systemd specific
