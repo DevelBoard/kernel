71a448dff79464089df94265e100a4477da20544
Mirko Damiani <mirko@develer.com>
2015-11-20 18:26:04 +0100

    buildroot: remove at91 nand header from at91bootstrap

--- buildroot/board/develer/develboard/post-build.sh
+++ buildroot/board/develer/develboard/post-build.sh
@@ -7,18 +7,12 @@ ROOTFSDIR="$BASE_DIR/target"
 OUTPUTDIR="$BASE_DIR/images"
 BOARDDIR="$PWD/board/develer/develboard"
 
-echo "Prepend AT91 nand flash header to bootstrap"
-"$BOARDDIR"/at91_nand_header.sh \
-	"$OUTPUTDIR"/sama5d4_xplained-nandflashboot-barebox-*.bin \
-	"$OUTPUTDIR"/.prefix-bootloader.bin
-
-mv "$OUTPUTDIR"/.prefix-bootloader.bin \
+mv "$OUTPUTDIR"/sama5d4_xplained-nandflashboot-barebox-*.bin \
     "$OUTPUTDIR"/develboard-nandflashboot-barebox.bin
 
 mv "$OUTPUTDIR"/sama5d4_xplained-sdcardboot-barebox-*.bin \
     "$OUTPUTDIR"/develboard-sdcardboot-barebox.bin
 
-rm "$OUTPUTDIR"/sama5d4_xplained-*.bin
 rm "$OUTPUTDIR"/at91bootstrap.bin
 
 mv "$OUTPUTDIR"/barebox-*-env "$OUTPUTDIR"/barebox.env || true
